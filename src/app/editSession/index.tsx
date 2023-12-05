c067ecd4208d8a10c95606db2cfe62840a36ebe2
carlos.eduardo2484417 <carlos.eduardo@startaideia.com.br>
2023-12-04 23:59:04 -0300

    feat: EditSession

---
+++ src/app/editSession/index.tsx
@@ -0,0 +1,10 @@
+import { View } from "react-native";
+import styles from "./styles.module.scss";
+
+export const EditSession: React.FC = (): React.ReactElement => {
+    return (
+        <View className={styles["container"]}>
+            
+        </View>
+    )
+}
